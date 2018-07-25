3c028bd20a510f6a7e29919625e0ef6a20ddafe4
Perchitti <LaurenPerchitti@gmail.com>
2018-07-25 12:26:21 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  #devise_for :users
+  devise_for :users
   root to: "users#index"
 
   namespace :v1 do
     resources :sessions, only: [:create, :destroy]
   end
-  resources :locations
-  resources :items
-  resources :users
-  resources :projects
+
+  resources :projects do
+    resources :locations
+    resources :items
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
