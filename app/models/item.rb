c921511173310f7728dc15f98d17840ca0e46dc8
Perchitti <LaurenPerchitti@gmail.com>
2018-07-25 12:26:14 -0400

    Added relationships to models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+  belongs_to :project
+  belongs_to :user
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,4 @@
 class Location < ApplicationRecord
+  has_many :projects
+  has_many :users, through: :projects
 end
