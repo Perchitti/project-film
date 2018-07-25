80b8e669f5d0aa887b8a70f427c465513c56a093
Perchitti <LaurenPerchitti@gmail.com>
2018-07-25 12:33:59 -0400

    Dropped equipment migration

---
+++ db/migrate/20180725163103_drop_equipment_table.rb
@@ -0,0 +1,15 @@
+class DropEquipmentTable < ActiveRecord::Migration[5.2]
+  def change
+    drop_table :equipment do |t|
+      t.text :make
+      t.text :model
+      t.text :type
+      t.text :condition
+      t.text :department
+      t.text :content
+      t.references :user, foregin_key: true
+
+      t.timestamps
+    end
+  end
+end
