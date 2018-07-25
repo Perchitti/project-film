d9d894eafdcb7cf313d9eff6d792059be365589a
Perchitti <LaurenPerchitti@gmail.com>
2018-07-25 12:34:09 -0400

    Updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :projects
   has_many :items
-  has_many :locations thought: :projects
+  has_many :locations, through: :projects
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_24_222658) do
+ActiveRecord::Schema.define(version: 2018_07_25_163103) do
 
   create_table "items", force: :cascade do |t|
     t.string "make"
